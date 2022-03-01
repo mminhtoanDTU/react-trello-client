f48bf733cb251cfbd04b0bedef7bf9933e99165d
Lê Minh Toàn <mmminhle771@gmail.com>
2022-03-01 21:14:14 +0700

    Design trello fontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react'
+import { BoardColumn, Header } from './components'
+import './App.scss'
+import { WorkPage } from './pages'
+
 function App() {
-    return <div className="App">Hello world </div>
+    return (
+        <div className="app theme-purple">
+            <Header />
+            <WorkPage />
+        </div>
+    )
 }
 
 export default App
